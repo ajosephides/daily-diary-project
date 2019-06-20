f989018d88bf59be4635c690071a066f220f6e40
Alexis Josephides <ajosephides@gmail.com>
2019-06-20 13:46:45 +0100

    removed some commented out code

--- spec/post_spec.rb
+++ spec/post_spec.rb
@@ -3,18 +3,6 @@ require 'pg'
 
 describe Post do
 
-  # subject(:post) {Post.new('Title 1', 'This is my first entry')}
-
-  # describe '#initialize' do
-  #   it 'is initialised with a title' do
-  #     expect(subject.title).to eq('Title 1')  
-  #   end
-
-  #   it 'is initialized with content' do
-  #     expect(subject.content).to eq('This is my first entry')
-  #   end
-  # end
-
   describe '#create' do
     it 'creates a new entry in a database' do
       first_post = Post.create(title: 'Title 1', content: 'This is my first entry')
