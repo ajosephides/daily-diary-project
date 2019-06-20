e9130e1291a7f5e2668df2257510d6c6ff5ce275
Alexis Josephides <ajosephides@gmail.com>
2019-06-20 12:16:17 +0100

    can save a diary entry to a database

--- lib/post.rb
+++ lib/post.rb
@@ -1,10 +1,18 @@
+require 'pg'
+
 class Post
 
   attr_reader :title, :content
 
-  def initialize(title, content)
+  def initialize(id, title, content)
+    @id = id
     @title = title
     @content = content
   end
 
+  def self.create(title, content)
+    db = PG.connect(dbname: 'daily_diary_test')
+    entry = db.exec("INSERT INTO posts (title, content) VALUES('#{title}', '#{content}') RETURNING id, title, content;")
+    Post.new(entry[0]['id'], entry[0]['title'], entry[0]['content'])
+  end  
 end
--- spec/post_spec.rb
+++ spec/post_spec.rb
@@ -1,16 +1,25 @@
 require 'post'
+require 'pg'
 
 describe Post do
 
-  subject(:post) {Post.new('Title 1', 'This is my first entry')}
+  # subject(:post) {Post.new('Title 1', 'This is my first entry')}
 
-  describe '#initialize' do
-    it 'is initialised with a title' do
-      expect(subject.title).to eq('Title 1')  
-    end
+  # describe '#initialize' do
+  #   it 'is initialised with a title' do
+  #     expect(subject.title).to eq('Title 1')  
+  #   end
 
-    it 'is initialized with content' do
-      expect(subject.content).to eq('This is my first entry')
+  #   it 'is initialized with content' do
+  #     expect(subject.content).to eq('This is my first entry')
+  #   end
+  # end
+
+  describe '#create' do
+    it 'creates a new entry in a database' do
+      first_post = Post.create('Title 1', 'This is my first entry')
+      expect(first_post.title).to eq('Title 1')
+      expect(first_post.content).to eq('This is my first entry')
     end
   end
 end
